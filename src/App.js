258c51737a8c25b4f5668ca80ef4fc60ec914a45
TuanDung111 <dung.vo20csehcmut@hcmut.edu.vn>
2022-12-09 15:34:03 +0700

    .

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Login from '../pages/Login.js';
+
+import './App.css';
+
+import Chat from './pages/Chat.js';
+
+function App() {
+  return (
+    <Chat></Chat>
+  );
+}
+
+export default App;
