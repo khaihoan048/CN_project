822a7398089dc941f772199b2ba9bea18f2ac22c
DucTien <ht.ductien@gmail.com>
2022-12-12 03:39:54 +0700

    authentication

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import io from 'socket.io-client'
 import ChatPage from './pages/ChatPage.js';
 import Home from './pages/Home.js';
-// import Register from './pages/Register.js';
-// import Login from './pages/Login.js';
+import Register from './pages/Register.js';
+import Login from './pages/Login.js';
 
 const socket = io.connect('http://103.195.236.149:5555')
 
@@ -13,7 +13,9 @@ function App() {
     <BrowserRouter>
         <div>
           <Routes>
-            <Route path="/" element={<ChatPage socket={socket}/>}></Route>
+            <Route path='/register' element={<Register/>}></Route>
+            <Route path='/Home' element={<Home socket={socket}/>}></Route>
+            <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
           </Routes>
     </div>
     </BrowserRouter>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,131 +1,86 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBSelect,
+  MDBRadio
+}
+from 'mdb-react-ui-kit';
 
-function Copyright(props) {
+function App() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <MDBContainer fluid>
+
+      <MDBRow className='justify-content-center align-items-center m-5'>
+
+        <MDBCard>
+          <MDBCardBody className='px-4'>
+
+            <h3 className="fw-bold mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
+
+            <MDBRow>
+
+              <MDBCol md='6'>
+                <MDBInput wrapperClass='mb-4' label='First Name' size='lg' id='form1' type='text'/>
+              </MDBCol>
+
+              <MDBCol md='6'>
+                <MDBInput wrapperClass='mb-4' label='Last Name' size='lg' id='form2' type='text'/>
+              </MDBCol>
+
+            </MDBRow>
+
+            <MDBRow>
+
+              <MDBCol md='6'>
+                <MDBInput wrapperClass='mb-4' label='Birthday' size='lg' id='form3' type='text'/>
+              </MDBCol>
+
+              <MDBCol md='6' className='mb-4'>
+                <h6 className="fw-bold">Gender: </h6>
+                <MDBRadio name='inlineRadio' id='inlineRadio1' value='option1' label='Female' inline />
+                <MDBRadio name='inlineRadio' id='inlineRadio2' value='option2' label='Male' inline />
+                <MDBRadio name='inlineRadio' id='inlineRadio3' value='option3' label='Other' inline />
+              </MDBCol>
+
+            </MDBRow>
+
+            <MDBRow>
+
+              <MDBCol md='6'>
+                <MDBInput wrapperClass='mb-4' label='Email' size='lg' id='form4' type='email'/>
+              </MDBCol>
+
+              <MDBCol md='6'>
+                <MDBInput wrapperClass='mb-4' label='Phone Number' size='lg' id='form5' type='rel'/>
+              </MDBCol>
+
+            </MDBRow>
+
+            <MDBSelect
+              label='Choose option'
+              className='mb-4'
+              size='lg'
+              data={[
+                { text: 'Choose option', value: 1, disabled: true },
+                { text: 'Subject 1', value: 2 },
+                { text: 'Subject 2', value: 3 },
+                { text: 'Subject 3', value: 4 }
+              ]}
+              />
+            <MDBBtn className='mb-4' size='lg'>Submit</MDBBtn>
+
+          </MDBCardBody>
+        </MDBCard>
+
+      </MDBRow>
+    </MDBContainer>
   );
 }
 
-const theme = createTheme();
-
-export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
-}
+export default App;
